37ef9f3fcc154a52dadd2c14e93f7a9121af364a
MicroKibaco <569015640@qq.com>
2018-03-15 16:54:25 +0800

    GreenDao 集成

---
+++ latte-ec/src/main/java/com/asiainfo/latte/ec/database/DatabaseManager.java
@@ -0,0 +1,38 @@
+package com.asiainfo.latte.ec.database;
+
+
+import android.content.Context;
+
+import org.greenrobot.greendao.database.Database;
+
+class DatabaseManager {
+
+    private UserProfileDao mDao = null;
+
+    private DatabaseManager() {
+    }
+
+    public static DatabaseManager getInstance() {
+        return Holder.INSTANCE;
+    }
+
+    public DatabaseManager init(Context context) {
+        initDao(context);
+        return this;
+    }
+
+    private void initDao(Context context) {
+        final ReleaseOpenHelper helper = new ReleaseOpenHelper(context, "fast_ec.db");
+        final Database db = helper.getWritableDb();
+        final DaoSession session = new DaoMaster(db).newSession();
+        mDao = session.getUserProfileDao();
+    }
+
+    private UserProfileDao getDao() {
+        return mDao;
+    }
+
+    private static final class Holder {
+        private static final DatabaseManager INSTANCE = new DatabaseManager();
+    }
+}
