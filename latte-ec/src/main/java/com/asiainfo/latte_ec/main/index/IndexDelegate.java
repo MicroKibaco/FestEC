b89c5acb7c561d8bfa9f6a0dbb075138c383d04c
github <569015640@qq.com>
2018-03-19 15:30:57 +0800

    创建首页UI

--- latte-ec/src/main/java/com/asiainfo/latte_ec/main/index/IndexDelegate.java
+++ latte-ec/src/main/java/com/asiainfo/latte_ec/main/index/IndexDelegate.java
@@ -2,10 +2,18 @@ package com.asiainfo.latte_ec.main.index;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.asiainfo.latte_core.delegates.bottom.BottomItemDelegate;
 import com.asiainfo.latte_ec.R;
+import com.asiainfo.latte_ec.R2;
+import com.joanzapata.iconify.widget.IconTextView;
+
+import butterknife.BindView;
 
 
 /**
@@ -13,6 +21,19 @@ import com.asiainfo.latte_ec.R;
  */
 
 public class IndexDelegate extends BottomItemDelegate {
+
+
+    @BindView(R2.id.srl_index)
+    SwipeRefreshLayout mRefreshLayout;
+    @BindView(R2.id.rv_index)
+    RecyclerView mRecyclerView;
+    @BindView(R2.id.tb_index)
+    Toolbar mToolbar;
+    @BindView(R2.id.icon_index_scan)
+    IconTextView mIconScan;
+    @BindView(R2.id.et_search_view)
+    AppCompatEditText mSearchView;
+
     @Override
     public Object setLayout() {
         return R.layout.delegate_index;
